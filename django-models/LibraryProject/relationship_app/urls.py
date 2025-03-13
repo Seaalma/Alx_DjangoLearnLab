d482cb5c0fa0675fa631c0a6219c3ead96b1f36d
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-13 17:34:17 +0000

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,25 +1,10 @@
-# views.py
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView, LogoutView
+# relationship_app/urls.py
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
-# Custom Login View
-class CustomLoginView(LoginView):
-    template_name = 'login.html'
-
-# Custom Logout View
-class CustomLogoutView(LogoutView):
-    next_page = 'login'
-
-# Registration View
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Log the user in immediately after registration
-            return redirect('home')  # Redirect to a logged-in user's home page
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+]
