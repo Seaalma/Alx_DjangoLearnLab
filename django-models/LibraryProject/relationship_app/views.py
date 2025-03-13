80f05d0bb00fa57849614d04c8af3d889c6aec67
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-13 17:32:38 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,5 @@
+ from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 from django.shortcuts import render
 from .models import Book
