edec9bea0b5ab99b388fb403dda8e395467cdc9e
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-13 18:38:45 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,26 @@
- from django.contrib.auth import login
+# relationship_app/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.detail import DetailView
-from django.shortcuts import render
-from .models import Book
+from django.contrib.auth.views import LoginView, LogoutView
 
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
-from django.views.generic import DetailView
-from .models import Library
+# Login View
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
 
-   context_object_name = "library"
-template_name = "relationship_app/library_detail.html"
+# Logout View
+class CustomLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
 
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "relationship_app/library_detail.html"
+# Registration View
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the newly registered user
+            return redirect("home")  # Redirect to a page after login
+    else:
+        form = UserCreationForm()
+
+    return render(request, "relationship_app/register.html", {"form": form})
