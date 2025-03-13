48836c6fc62e69959c9b472fd5c2839f4c84f2f6
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-13 22:12:11 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Function to check if the user is an Admin
+# Function to check if user is an Admin
 def is_admin(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-# Admin-only view (inside views.py)
+# View restricted to Admins
 @login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, "relationship_app/admin_view.html")
+
