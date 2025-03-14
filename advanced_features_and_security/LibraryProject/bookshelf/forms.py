230febdf3a5165f4578442694ea3eec0415e711c
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-14 03:51:41 +0000

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,9 @@
-<!-- Example: book_list.html or form_example.html -->
-<form method="POST">
-    {% csrf_token %}
-    <!-- Other form fields go here -->
-    <input type="submit" value="Submit">
-</form>
+# bookshelf/forms.py
+
+from django import forms
+from .models import Book  # If you want to create a form for the Book model
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book  # You can replace this with the model you are using
+        fields = ['title', 'author']  # Include fields you want in the form
