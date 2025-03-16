adb8593af43bbbefe8c369650dab5728ee33c912
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-16 02:50:31 +0000

    Update urls.py

--- advanced-api-project/advanced_api_project/api/urls.py
+++ advanced-api-project/advanced_api_project/api/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', views.BookListView.as_view(), name='book-list'),  # List all books
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # Get a single book by ID
-    path('books/create/', views.BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Make sure this line is present
 ]
-
-
