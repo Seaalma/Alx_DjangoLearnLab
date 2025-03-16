233a9800e1832c5c841e56722bdf08420b48eef8
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-16 00:44:54 +0000

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,14 +1,5 @@
-from django.urls import path
-from . import views
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
-urlpatterns = [
-    path('books/', views.BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', views.BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),  # Ensure this is correct
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),  # Ensure this is correct
-]
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
