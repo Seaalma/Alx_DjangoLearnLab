a871bb46d4f893c041d5ea029d1ec9e1bbb30bf1
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-16 00:35:57 +0000

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    isbn_number = models.CharField(max_length=13, unique=True)
+    pages = models.IntegerField()
+    language = models.CharField(max_length=50)
 
     def __str__(self):
         return self.title
