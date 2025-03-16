a80e01f8fe3e41bade93e49fd1bade707de5bfcf
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-16 05:27:11 +0000

    Update settings.py

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -37,9 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'api',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+}
+    
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
