8ce4f2062d6ee4ac8af3ca78f85647f6e75de066
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-16 17:29:45 +0000

    Update settings.py

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -90,8 +90,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ':memory:',  # Runs tests in RAM to avoid modifying your real DB
     }
 }
+
 
 
 # Password validation
