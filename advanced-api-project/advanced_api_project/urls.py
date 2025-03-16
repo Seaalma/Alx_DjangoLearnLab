2e53d1d150c255e209ca33e586593ad7977868da
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-16 03:19:01 +0000

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include the API routes
+    path('api/', include('api.urls')),  # Include all API routes
 ]
