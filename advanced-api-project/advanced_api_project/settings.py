5b4928c446944e4b358015e8f09304e67d1fb472
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-16 05:48:54 +0000

    Update settings.py

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
-    'rest_framework',
     'api',
+    'rest_framework',
+    
 ]
 
 REST_FRAMEWORK = {
