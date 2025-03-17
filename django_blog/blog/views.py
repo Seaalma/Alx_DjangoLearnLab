48878fcd962e715a2504dd822fc33ff539592d8f
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-17 02:29:24 +0000

    Create views.py

---
+++ django_blog/blog/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Login view
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# Profile view
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
