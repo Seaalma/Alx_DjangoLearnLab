de2e6004c9f9e5ca098d8c18920242832637761b
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-17 00:08:58 +0000

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include all API routes
+    path("books/", ListView.as_view(), name="book-list"),
+    path("books/create/", CreateView.as_view(), name="book-create"),
+    path("books/update/<int:pk>/", UpdateView.as_view(), name="book-update"),  # Ajustement ici
+    path("books/delete/<int:pk>/", DeleteView.as_view(), name="book-delete"),  # Ajustement ici
+    path("books/<int:pk>/", DetailView.as_view(), name="book-detail"),
 ]
