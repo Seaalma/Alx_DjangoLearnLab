cd54b8c875499851d7e28a9805762de826eaccd5
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-17 00:07:12 +0000

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import ListView, DetailView, UpdateView, DeleteView
-from django.urls import include, path
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
-    path("api/", include("api.urls")),  # Assure-toi que "api" est bien inclus]
     path("books/", ListView.as_view(), name="book-list"),
+    path("books/create/", CreateView.as_view(), name="book-create"),  # Ajout de la route CreateView
     path("books/<int:pk>/", DetailView.as_view(), name="book-detail"),
     path("books/<int:pk>/update/", UpdateView.as_view(), name="book-update"),
     path("books/<int:pk>/delete/", DeleteView.as_view(), name="book-delete"),
