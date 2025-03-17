e6384cb427eed605bd52216ca4e614f962aa5b67
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-17 00:08:41 +0000

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,9 +3,10 @@ from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
     path("books/", ListView.as_view(), name="book-list"),
-    path("books/create/", CreateView.as_view(), name="book-create"),  # Ajout de la route CreateView
+    path("books/create/", CreateView.as_view(), name="book-create"),
+    path("books/update/<int:pk>/", UpdateView.as_view(), name="book-update"),  # Ajustement ici
+    path("books/delete/<int:pk>/", DeleteView.as_view(), name="book-delete"),  # Ajustement ici
     path("books/<int:pk>/", DetailView.as_view(), name="book-detail"),
-    path("books/<int:pk>/update/", UpdateView.as_view(), name="book-update"),
-    path("books/<int:pk>/delete/", DeleteView.as_view(), name="book-delete"),
 ]
+
 
