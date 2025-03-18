c593fa666bcfe4fd7e8df23019b9419371e36473
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-18 23:24:54 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
-class Comment(models.Model):
-    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")  # Links to Post model
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Links to User model
-    content = models.TextField()  # Stores comment text
-    created_at = models.DateTimeField(auto_now_add=True)  # Auto-filled when created
-    updated_at = models.DateTimeField(auto_now=True)  # Auto-updated when edited
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    tags = TaggableManager()  # Ajout de la gestion des tags
 
     def __str__(self):
-        return f"Comment by {self.author} on {self.post}"
+        return self.title
