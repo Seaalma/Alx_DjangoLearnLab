1e7bce6b31330a1df79a4d914caea758364bd542
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-18 16:37:02 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
+from django.utils.timezone import now
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    date_posted = models.DateTimeField(default=now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
