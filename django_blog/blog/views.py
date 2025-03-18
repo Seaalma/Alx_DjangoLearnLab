ae66ff087eb585b64287e60cecbcaaf2f6227262
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-18 23:08:04 +0000

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,44 +1,17 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-from django.views.generic import UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Comment
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Comment
 from .forms import CommentForm
 
-@login_required
-def add_comment(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            return redirect("post-detail", pk=post_id)
-    else:
-        form = CommentForm()
-    return render(request, "blog/add_comment.html", {"form": form})
-
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
-    fields = ["content"]
-    template_name = "blog/edit_comment.html"
+    form_class = CommentForm
+    template_name = "blog/add_comment.html"
 
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author  # Ensures only the author can edit
-
-    def get_success_url(self):
-        return self.object.post.get_absolute_url()
-
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Comment
-    template_name = "blog/delete_comment.html"
-
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author  # Ensures only the author can delete
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Assign logged-in user as author
+        form.instance.post_id = self.kwargs["post_id"]  # Assign post ID from URL
+        return super().form_valid(form)
 
     def get_success_url(self):
         return self.object.post.get_absolute_url()
