e433274ac6e5d332ab2b6adddd83b9f03ced112e
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-18 16:56:22 +0000

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,56 +1,49 @@
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
 
-# ✅ Liste des articles
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
-    ordering = ['-date_posted']
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comments = post.comments.all()
+    form = CommentForm()
 
-# ✅ Détail d'un article
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
 
-# ✅ Création d'un article (réservé aux utilisateurs authentifiés)
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    fields = ['title', 'content']
-    template_name = 'blog/post_form.html'
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = CommentForm()
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+    return render(request, 'blog/add_comment.html', {'form': form, 'post': post})
 
-# ✅ Mise à jour d'un article (seulement par l'auteur)
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    fields = ['title', 'content']
-    template_name = 'blog/post_form.html'
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id=comment.post.id)
+    else:
+        form = CommentForm(instance=comment)
 
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
+    return render(request, 'blog/edit_comment.html', {'form': form, 'comment': comment})
 
-# ✅ Suppression d'un article (seulement par l'auteur)
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post-list')
-
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-        @login_required
-def dummy_view(request):
-    return None
-
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+    post_id = comment.post.id
+    comment.delete()
+    return redirect('post_detail', post_id=post_id)
