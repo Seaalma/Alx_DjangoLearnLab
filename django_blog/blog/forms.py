075cc5b5a19865ee7b894d2ad3f1c231a8fd0f6e
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-19 00:04:29 +0000

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from .models import Post
+from taggit.forms import TagWidget  # Ajoutez cette ligne
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(widget=TagWidget())  # Utilisation du widget correct
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
