795deb204b66d3a1b8423f225a19b869a0c4c7f6
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-19 03:06:23 +0000

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-
+from . import views
 urlpatterns = [
     # URL for listing all posts
     path('post/', PostListView.as_view(), name='post-list'),
@@ -16,4 +16,9 @@ urlpatterns = [
     
     # URL for deleting a post
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('posts/<int:post_id>/comments/new/', views.post_detail, name='add_comment'),
+    path('comments/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
+    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
 ]
+
