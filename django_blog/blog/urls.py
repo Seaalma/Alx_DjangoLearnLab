dfdce81562a61c673c8782b1918a0e6684d75269
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-19 03:23:51 +0000

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit_comment'),
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
      path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-    
     # Create a new comment
     path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='add_comment'),
     
