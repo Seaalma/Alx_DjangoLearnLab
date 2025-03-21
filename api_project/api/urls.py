d1e7188478fd3e98e0d17de223695d261a6b5528
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-21 16:39:43 +0000

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookList
+from .views import BookList  # Ensure BookList is imported
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
