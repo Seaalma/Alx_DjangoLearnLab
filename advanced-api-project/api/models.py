aa67971d17328cc0cab9915f64bd840af223f1f4
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-24 04:04:30 +0000

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 class Author(models.Model)
+"name", 
+"publication_year",
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
@@ -7,6 +9,7 @@ class Book(models.Model):
     isbn_number = models.CharField(max_length=13, unique=True)
     pages = models.IntegerField()
     language = models.CharField(max_length=50)
+    
 
     def __str__(self):
         return self.title
