b10af0860e99dbc582701f94499355ffd3464971
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-24 03:28:46 +0000

    Create views.py

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets, permissions
+from rest_framework.pagination import PageNumberPagination
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+class PostPagination(PageNumberPagination):
+    page_size = 5
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    pagination_class = PostPagination
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
