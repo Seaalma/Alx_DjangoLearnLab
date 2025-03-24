68ca68bbe5e8e429f824b90d562e388ac5cb2844
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-24 04:05:27 +0000

    Update serializers.py

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Book
-
+serializers.ValidationError,
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
