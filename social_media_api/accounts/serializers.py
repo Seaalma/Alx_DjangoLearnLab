b2a328db2c7ceace3e9fe6b73d222569543e7368
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-24 03:01:32 +0000

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,18 +5,16 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    serializers.CharField()",
-    "Token.objects.create", 
-    
+"Token.objects.create",
     class Meta:
-        model = User
+        model = get_user_model()  # ✅ Ensures dynamic User model support
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        "get_user_model().objects.create_use
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(  # ✅ This line must exist!
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
         return user
+
