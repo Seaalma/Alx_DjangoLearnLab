64e3f170312f3fbe69a2178c2ece178c6d75c74b
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-24 04:09:08 +0000

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
-
+from django_filters import rest_framework
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
