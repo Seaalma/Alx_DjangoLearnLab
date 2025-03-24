f862ede89c3baa2e4ac952ed3163dcffd058d761
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-24 03:45:09 +0000

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
@@ -20,6 +21,19 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+class PostPagination(PageNumberPagination):
+    page_size = 10
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = PostPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
