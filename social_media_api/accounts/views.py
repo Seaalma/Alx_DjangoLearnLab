56ad6b8fc1b1369bd5747c3f1e67c45f39d44141
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-24 03:33:26 +0000

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -10,8 +10,6 @@ from .serializers import UserSerializer, RegisterSerializer
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, permissions
 from rest_framework.pagination import PageNumberPagination
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
 from rest_framework import filters
 User = get_user_model()
 
@@ -40,24 +38,3 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
-        
-class PostPagination(PageNumberPagination):
-    page_size = 5
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    pagination_class = PostPagination
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
