38ac19c3cd7f05c9df0929a568dbde6b91d04a7e
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-25 03:46:30 +0000

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
+from .views import like_post, unlike_post
 feed/
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -8,4 +9,6 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+     path('<int:pk>/like/', like_post, name='like_post'),
+    path('<int:pk>/unlike/', unlike_post, name='unlike_post'),
 ]
