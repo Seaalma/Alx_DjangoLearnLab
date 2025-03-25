359ddacaee378902f115cb34b622d6f2245f48be
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-25 03:37:40 +0000

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.contrib.auth.models import User
 User = get_user_model()
 
 class Post(models.Model):
@@ -9,7 +9,9 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
 
@@ -22,3 +24,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+        
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Empêche un utilisateur d'aimer un post plusieurs fois.
